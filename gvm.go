29d13a60fa6d458dc0254332dc8a6c251d2aefec
aulsai <witraphee@gmail.com>
2012-07-24 20:50:16 +0700

    Update gvm.go

--- gvm.go
+++ gvm.go
@@ -6,22 +6,22 @@ import ( "fmt"
 		"encoding/binary"
 )
 type classfile struct{
-	magic 									uint32
+	magic 								uint32
 	minor_version 							uint16
 	major_version 							uint16
 	constant_pool_count						uint16
 	constant_pool							[]cp_info
 	access_flags							uint16
-	this_class								uint16
-	super_class								uint16
+	this_class							uint16
+	super_class							uint16
 	interfaces_count						uint16
-	interfaces								[]uint16
+	interfaces							[]uint16
 	fields_count							uint16
-	fields									[]field_info
+	fields								[]field_info
 	methods_count							uint16
-	//methods								[]method_info
+	//methods							[]method_info
 	attributes_count						uint16
-	//attributes[attributes_count]			*attributes_info
+	//attributes[attributes_count]					*attributes_info
 }	
 
 const (
@@ -31,57 +31,57 @@ const (
     ACC_INTERFACE   = 0x0200
     ACC_ABSTRACT    = 0x0400
 )
-const(	//ACC_PUBLIC 		= 0x0001 	
+const(		//ACC_PUBLIC 	= 0x0001 	
 		ACC_PRIVATE 	= 0x0002 	
 		ACC_PROTECTED 	= 0x0004 	
-		ACC_STATIC 		= 0x0008 
-		//ACC_FINAL 		= 0x0010 	
+		ACC_STATIC 	= 0x0008 
+		//ACC_FINAL 	= 0x0010 	
 		ACC_VOLATILE 	= 0x0040 	 
 		ACC_TRANSIENT 	= 0x0080 	
 		ACC_SYNTHETIC 	= 0x1000 	
-		ACC_ENUM 		= 0x4000
+		ACC_ENUM 	= 0x4000
 )
 type cp_info struct{
-	tag		uint8
+	tag	uint8
 	info 	[]uint8
 }
 type field_info struct {
     access_flags 	uint16
-    name_index      uint16
+    name_index      	uint16
     descriptor_index	uint16
     attributes_count	uint16
-    attributes			[]attribute_info 	//[attributes_count]
+    attributes		[]attribute_info 	//[attributes_count]
 }
 type attribute_info struct{
     attribute_name_index	uint16
     attribute_length		uint32
-    info					[]uint8	//[attribute_length]
+    info			[]uint8	//[attribute_length]
 }
 
 type CONSTANT_Class_info struct{
-	tag			uint8
+	tag		uint8
 	name_index	uint16
 }
 
 type CONSTANT_Fieldref_info struct{
-    tag 					uint8
-    class_index				uint16
+    tag 			uint8
+    class_index			uint16
     name_and_type_index		uint16
 }
 type CONSTANT_Methodref_info struct{
-    tag					uint8
+    tag				uint8
     class_index			uint16
     name_and_type_index	uint16
 }
 
 type CONSTANT_InterfaceMethodref_info struct{
-    tag					uint8
+    tag				uint8
     class_index			uint16
-    name_and_type_index	uint16
+    name_and_type_index		uint16
 }
 
 type CONSTANT_String_info struct{
-    tag				uint8
+    tag			uint8
     string_index	uint16
 }
 
@@ -102,53 +102,53 @@ type CONSTANT_Long_info struct{
 }
 
 type CONSTANT_Double_info struct{
-    tag			uint8
+    tag		uint8
     high_bytes	uint32
     low_bytes	uint32
 }
 
 type CONSTANT_NameAndType_info struct{
-    tag					uint8
-    name_index 			uint16
+    tag			uint8
+    name_index 		uint16
     descriptor_index	uint16
 }
 
 type CONSTANT_Utf8_info struct{
-    tag				uint8
-    length			uint16
-    bytes 			[]uint8
+    tag			uint8
+    length		uint16
+    bytes 		[]uint8
 }
 
 type CONSTANT_MethodHandle_info struct{
-    tag					uint8
+    tag				uint8
     reference_kind		uint8
     reference_index		uint16
 }
 type CONSTANT_MethodType_info struct{
-    tag					uint8
+    tag			uint8
     descriptor_index 	uint16
 }
 type CONSTANT_InvokeDynamic_info struct{
-    tag							uint8
-    bootstrap_method_attr_index	uint16
+    tag					uint8
+    bootstrap_method_attr_index		uint16
     name_and_type_index			uint16
 }
 
 
-const (	CONSTANT_Class				=	7
+const (		CONSTANT_Class				=	7
 		CONSTANT_Fieldref			=	9
 		CONSTANT_Methodref			=	10
-		CONSTANT_InterfaceMethodref	=	11
+		CONSTANT_InterfaceMethodref		=	11
 		CONSTANT_String				=	8
 		CONSTANT_Integer			=	3
 		CONSTANT_Float				=	4
 		CONSTANT_Long				=	5
 		CONSTANT_Double				=	6
-		CONSTANT_NameAndType		=	12
+		CONSTANT_NameAndType			=	12
 		CONSTANT_Utf8				=	1
-		CONSTANT_MethodHandle		=	15
+		CONSTANT_MethodHandle			=	15
 		CONSTANT_MethodType			=	16
-		CONSTANT_InvokeDynamic		=	18
+		CONSTANT_InvokeDynamic			=	18
 )
 
 func readMagic(file *os.File,cf classfile) {
@@ -159,8 +159,7 @@ func readMagic(file *os.File,cf classfile) {
 func readVersion(file *os.File,cf classfile) {
     binary.Read(file, binary.BigEndian, &(cf.minor_version))
     binary.Read(file, binary.BigEndian, &(cf.major_version))
-  //   fmt.Printf("minor version = %d\n", cf.minor_version)
-     fmt.Printf("Version = %d.%d\n", cf.major_version,cf.minor_version)
+    fmt.Printf("Version = %d.%d\n", cf.major_version,cf.minor_version)
 }
 
 func readConstantPool(file *os.File,cf *classfile) {
@@ -174,45 +173,21 @@ func readConstantPool(file *os.File,cf *classfile) {
 		binary.Read(file,binary.BigEndian,&(tag))
 			switch tag { 
 				case CONSTANT_Class: fallthrough
-					//	info := make([]byte, 2)		 
-					//	binary.Read(file,binary.BigEndian,info)
-					//	cf.constant_pool[i]=cp_info{tag:tag, info:info }
 				case CONSTANT_MethodType: fallthrough
-					//	info := make([]byte, 2)
-					//	binary.Read(file,binary.BigEndian,info)
-					//	cf.constant_pool[i]=cp_info{tag:tag, info:info }
 				case CONSTANT_String:
 						info := make([]byte, 2)
 						binary.Read(file,binary.BigEndian,info)
 						cf.constant_pool[i]=cp_info{tag:tag, info:info }
 				case CONSTANT_Fieldref: fallthrough
-					/*	info := make([]byte, 4)
-						binary.Read(file,binary.BigEndian,info)
-						cf.constant_pool[i]=cp_info{tag:tag, info:info } */
 				case CONSTANT_Methodref: fallthrough
-					/*	info := make([]byte, 4)
-						binary.Read(file,binary.BigEndian,info)
-						cf.constant_pool[i]=cp_info{tag:tag, info:info } */
 				case CONSTANT_InterfaceMethodref: fallthrough
-					//	info := make([]byte, 4)
-					//	binary.Read(file,binary.BigEndian,info)
-					//	cf.constant_pool[i]=cp_info{tag:tag, info:info }
 				case CONSTANT_Integer: fallthrough
-					//	info := make([]byte, 4)
-					//	binary.Read(file,binary.BigEndian,info)
-					//	cf.constant_pool[i]=cp_info{tag:tag, info:info }
 				case CONSTANT_Float: fallthrough
-					//	info := make([]byte, 4)
-					//	binary.Read(file,binary.BigEndian,info)
-					//	cf.constant_pool[i]=cp_info{tag:tag, info:info }
 				case CONSTANT_NameAndType:
 						info := make([]byte, 4)
 						binary.Read(file,binary.BigEndian,info)
 						cf.constant_pool[i]=cp_info{tag:tag, info:info }
 				case CONSTANT_Long:	fallthrough
-					//	info := make([]byte, 8)
-					//	binary.Read(file,binary.BigEndian,info)
-					//	cf.constant_pool[i]=cp_info{tag:tag, info:info }
 				case CONSTANT_Double:
 						info := make([]byte, 8)
 						binary.Read(file,binary.BigEndian,info)
@@ -304,7 +279,7 @@ func openfile(filename string,cf classfile) {
 			fmt.Printf("%v", err)	
 		}
 	defer f.Close()
-	//var cc CONSTANT_Class_info
+
 	checkSize(f)
     readMagic(f,cf)
     readVersion(f,cf)
