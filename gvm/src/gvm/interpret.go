1e600708b02b0e25fcb2bdc95f0aae460758b0a7
Sarawut Eiamsumang <sarawut_cpe@windowslive.com>
2012-08-20 13:26:49 +0700

    implement new, dup, invokespecial

--- gvm/src/gvm/interpret.go
+++ gvm/src/gvm/interpret.go
@@ -1,6 +1,7 @@
 package gvm
 
 import "fmt"
+import "strings"
 import "encoding/binary"
 
 func Interpret(ca code_attribute, cf *ClassFile) {
@@ -9,12 +10,13 @@ func Interpret(ca code_attribute, cf *ClassFile) {
     locals := make([]interface{}, ca.max_locals)
     code := ca.code
     pc := 0
+    strBuilder := ""
 
     for {
         op := code[pc]
         switch op {
             case LDC:
-                //fmt.Println(code[pc], code[pc+1])
+                //fmt.Println("@LDC", code[pc], code[pc+1])
                 if len(cf.constant_pool[code[pc+1]].info) == 2 {
                     s.Push(int(binary.BigEndian.Uint16(cf.constant_pool[code[pc+1]].info)))
                 }else if len(cf.constant_pool[code[pc+1]].info) == 4 {
@@ -23,6 +25,7 @@ func Interpret(ca code_attribute, cf *ClassFile) {
                     //s.Push(cf.constant_pool[code[pc+1]].info)
                 }
                 pc = pc + 2
+
             case ICONST_M1:
                 s.Push(-1)
                 pc++
@@ -46,6 +49,7 @@ func Interpret(ca code_attribute, cf *ClassFile) {
                 pc++
             ////case ASTORE_1:
             ////    locals[1] =
+
             case ISTORE:
                 locals[code[pc+1]] = s.Pop()
                 pc = pc + 2
@@ -58,6 +62,7 @@ func Interpret(ca code_attribute, cf *ClassFile) {
             case ISTORE_3:
                 locals[3] = s.Pop()
                 pc++
+
             case ILOAD:
                 s.Push(locals[code[pc+1]])
                 pc = pc + 2
@@ -70,6 +75,7 @@ func Interpret(ca code_attribute, cf *ClassFile) {
             case ILOAD_3:
                 s.Push(locals[3])
                 pc++
+
             case BIPUSH:
                 s.Push(int(code[pc+1]))
                 pc = pc + 2
@@ -104,7 +110,7 @@ func Interpret(ca code_attribute, cf *ClassFile) {
                 getb := []byte{code[pc+1], code[pc+2]}
                 value := binary.BigEndian.Uint16(getb)
                 if cf.constant_pool[value].tag == CONSTANT_Fieldref {
-                    fmt.Print("CONSTANT_Fieldref : ")
+                    //fmt.Print("CONSTANT_Fieldref : ")
                     //fmt.Println("fieldref=", cf.constant_pool[value].info)
                     //fmt.Println("class_index=", binary.BigEndian.Uint16(cf.constant_pool[value].info[:2]))
                     //fmt.Println("name_and_type_index=", binary.BigEndian.Uint16(cf.constant_pool[value].info[2:]))
@@ -117,18 +123,75 @@ func Interpret(ca code_attribute, cf *ClassFile) {
                 methodRef := cf.constant_pool[value].info
                 //class := binary.BigEndian.Uint16(methodRef[:2])
                 nameAndType := binary.BigEndian.Uint16(methodRef[2:])
-
                 method := string(cf.constant_pool[binary.BigEndian.Uint16(cf.constant_pool[nameAndType].info[:2])].info[2:])
                 signature := string(cf.constant_pool[binary.BigEndian.Uint16(cf.constant_pool[nameAndType].info[2:])].info[2:])
+                //obj := s.Pop().(int)
 
-                str := s.Pop().(int)
-                varType := cf.constant_pool[str].tag
+                if method == "append" {
+                    if strings.HasPrefix(signature, "(I)") {
+                        strBuilder = strBuilder + "14"
+                    }
+                    if strings.HasPrefix(signature, "(Ljava/lang/String;)") {
+                        obj := s.Pop().(int)
+                        strBuilder = strBuilder + string(cf.constant_pool[obj].info[2:])
+                    }
+                    pc = pc + 3
+                    break
+                }
 
-                if method == "println" && signature == "(Ljava/lang/String;)V" && varType == CONSTANT_Utf8 {
-                    fmt.Println(string(cf.constant_pool[str].info[2:]))
+                if method == "println" && signature == "(I)V" {
+                    obj := s.Pop().(int)
+                    fmt.Println(obj)
+                    pc = pc + 3
+                    break
+                }
+
+                if method == "println" && signature == "(Ljava/lang/String;)V" {
+                    if strBuilder == "" {
+                        obj := s.Pop().(int)
+                        varType := cf.constant_pool[obj].tag
+                        if varType == CONSTANT_Utf8 {
+                            fmt.Println(string(cf.constant_pool[obj].info[2:]))
+                            pc = pc + 3
+                            return
+                        }
+                    }else{
+                        fmt.Println(strBuilder)
+                    }
+
                 }
 
                 pc = pc + 3
+
+            case INVOKESPECIAL:
+                getb := []byte{code[pc+1], code[pc+2]}
+                value := binary.BigEndian.Uint16(getb)
+                methodRef := cf.constant_pool[value].info
+                //class := binary.BigEndian.Uint16(methodRef[:2])       //java/lang/StringBuilder
+                nameAndType := binary.BigEndian.Uint16(methodRef[2:])
+                method := string(cf.constant_pool[binary.BigEndian.Uint16(cf.constant_pool[nameAndType].info[:2])].info[2:])
+                signature := string(cf.constant_pool[binary.BigEndian.Uint16(cf.constant_pool[nameAndType].info[2:])].info[2:])
+                obj := s.Pop()
+
+                if method == "<init>" && signature == "()V" {
+                    fmt.Println(obj)
+                }
+
+                pc = pc + 3
+
+            case NEW:
+                getb := []byte{code[pc+1], code[pc+2]}
+                value := binary.BigEndian.Uint16(getb)
+                //fmt.Println(string(cf.constant_pool[23].info[2:]))
+                types := binary.BigEndian.Uint16(cf.constant_pool[value].info)
+                s.Push(types)
+                pc = pc + 3
+
+            case DUP:
+                obj := s.Pop()
+                s.Push(obj)
+                s.Push(obj)
+                pc++
 
             case RETURN:
                 fmt.Println(locals)
