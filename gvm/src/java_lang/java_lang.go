badc0cfffac765cc3df67122fb56f469bf0dd742
Sarawut Eiamsumang <sarawut_cpe@windowslive.com>
2012-08-27 17:58:38 +0700

    re-init, re-toString

--- gvm/src/java_lang/java_lang.go
+++ gvm/src/java_lang/java_lang.go
@@ -11,7 +11,8 @@ func Init(ct map[string]*Class) {
     java_lang_System.StaticFields["out"] = &Object{ClassName: "java/io/PrintStream", Native: os.Stdout}
 
     java_lang_StringBuilder := NewClass()
-    java_lang_StringBuilder.StaticFields["<init>()V"] = &Object{ClassName: "java/lang/StringBuilder", Native: ""}
+    //java_lang_StringBuilder.StaticFields["<init>()V"] = &Object{ClassName: "java/lang/StringBuilder", Native: ""}
+    java_lang_StringBuilder.Methods["<init>()V"] = &init__v{native: true}
     java_lang_StringBuilder.Methods["append(Ljava/lang/String;)Ljava/lang/StringBuilder;"] = &append__Ljava_lang_String__Ljava_lang_StringBuilder{native: true}
     java_lang_StringBuilder.Methods["append(I)Ljava/lang/StringBuilder;"] = &append__I__Ljava_lang_StringBuilder{native: true}
     java_lang_StringBuilder.Methods["toString()Ljava/lang/String;"] = &toString__V__Ljava_lang_String{native: true}
@@ -53,7 +54,21 @@ type toString__V__Ljava_lang_String struct {
 }
 
 func (m *toString__V__Ljava_lang_String) Invoke(recv *Object, args []*Object) (void bool, ret interface{}) {
-    return false, recv
+    obj := &Object{ClassName: "java/lang/String", Native: recv.Native.(string)}
+    return false, obj
 }
 
 func (m *toString__V__Ljava_lang_String) GetArgCount() int { return 0 }
+
+//**************************************************
+
+type init__v struct {
+    native bool
+}
+
+func (m *init__v) Invoke(recv *Object, args []*Object) (void bool, ret interface{}) {
+    recv.Native = ""
+    return true, nil
+}
+
+func (m *init__v) GetArgCount() int { return 0 }
